3f063a2a0cd8846e2558e5d07232802b41b9fe75
jie7206 <1524556052@qq.com>
2019-10-26 14:30:25 +0800

    #109[系统层]利息能实现编辑与删除功能遇错能显示错误讯息

--- spec/factories/interests.rb
+++ spec/factories/interests.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
 
   factory :interest do
 
-    association :property, :twd
+    association :property, :twd_loan
     start_date { "2019-10-01".to_date }
     rate { 4.50 }
 
--- spec/system/interests_spec.rb
+++ spec/system/interests_spec.rb
@@ -32,6 +32,111 @@ RSpec.describe '系统测试(Interests)', type: :system do
 
     end
 
+    describe '失败用例' do
+
+      specify '#109[系统层]当利息没有起算日时无法新建且显示错误讯息' do
+        fill_in 'interest[start_date]', with: nil
+        find('#create_new_interest').click
+        expect(page).to have_content $interest_start_date_blank_err
+      end
+
+      specify '#109[系统层]当利息没有利率值时无法新建且显示错误讯息' do
+        fill_in 'interest[rate]', with: nil
+        find('#create_new_interest').click
+        expect(page).to have_content $interest_rate_blank_err
+      end
+
+      specify '#109[系统层]当起算日格式不符时无法新建且显示错误讯息' do
+        fill_in 'interest[start_date]', with: 'abcdefg'
+        find('#create_new_interest').click
+        expect(page).to have_content $interest_start_date_type_err
+      end
+
+      specify '#109[系统层]当利息为负数时无法新建且显示错误讯息' do
+        fill_in 'interest[rate]', with: 'abcd'
+        find('#create_new_interest').click
+        expect(page).to have_content $interest_rate_type_err
+        fill_in 'interest[rate]', with: -3.5
+        find('#create_new_interest').click
+        expect(page).to have_content $interest_rate_type_err
+      end
+
+    end
+
+  end
+
+  describe '修改与删除利息' do
+
+    let!(:interest) { create(:interest) }
+    let!(:ori_interest_name) { interest.property.name }
+
+    before do
+      visit interests_path
+      click_on interest.property.name
+    end
+
+    describe '成功用例' do
+
+      specify '#109[系统层]能通过表单修改对应的资产' do
+        select '人民币贷款', from: 'interest[property_id]'
+        find('#update_interest').click
+        expect(page).to have_content '人民币贷款'
+        expect(page).to have_selector '.alert-notice'
+      end
+
+      specify '#109[系统层]能通过表单修改起算日' do
+        fill_in 'interest[start_date]', with: '2019-10-10'
+        find('#update_interest').click
+        expect(page).to have_content '2019-10-10'
+        expect(page).to have_selector '.alert-notice'
+      end
+
+      specify '#109[系统层]能通过表单修改利率值' do
+        fill_in 'interest[rate]', with: 2.8
+        find('#update_interest').click
+        expect(page).to have_content 2.8
+        expect(page).to have_selector '.alert-notice'
+      end
+
+      specify '#109[系统层]能通过表单删除一笔利息记录' do
+        find('#delete_interest').click
+        expect(page).not_to have_content ori_interest_name
+        expect(page).to have_selector '.alert-notice'
+      end
+
+    end
+
+    describe '失败用例' do
+
+      specify '#109[系统层]当利息没有起算日时无法新建且显示错误讯息' do
+        fill_in 'interest[start_date]', with: nil
+        find('#update_interest').click
+        expect(page).to have_content $interest_start_date_blank_err
+      end
+
+      specify '#109[系统层]当利息没有利率值时无法新建且显示错误讯息' do
+        fill_in 'interest[rate]', with: nil
+        find('#update_interest').click
+        expect(page).to have_content $interest_rate_blank_err
+      end
+
+      specify '#109[系统层]当起算日格式不符时无法新建且显示错误讯息' do
+        fill_in 'interest[start_date]', with: 'abcdefg'
+        find('#update_interest').click
+        expect(page).to have_content $interest_start_date_type_err
+      end
+
+      specify '#109[系统层]当利息为负数时无法新建且显示错误讯息' do
+        fill_in 'interest[rate]', with: 'abcd'
+        find('#update_interest').click
+        expect(page).to have_content $interest_rate_type_err
+        fill_in 'interest[rate]', with: -3.5
+        find('#update_interest').click
+        expect(page).to have_content $interest_rate_type_err
+      end
+
+    end
+
   end
 
 end
