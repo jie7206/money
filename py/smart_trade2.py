7b35430eded4a9a92ea99caadc01254288db529c
jie7206 <1524556052@qq.com>
2020-04-17 23:34:18 +0800

    #304定投参数新增'成本均价是否越买越低'并对两种买入策略皆可适用(Fixed)

--- py/smart_trade2.py
+++ py/smart_trade2.py
@@ -1705,8 +1705,7 @@ if __name__ == '__main__':
                                             is_buy_price = False
                                         str = "%s:%.2f %imin:%.2f sp:%i buy:%s sell:%s over_max: %s reach_p(%.2f:%.2f): %s lower: %s    " % (get_now(), price_now, buy_price_period, min_price, force_sell_price, over_buy_time, over_sell_time, over_max_buy_price, TRACE_MIN_PRICE, TRACE_MIN_PRICE*TRACE_MIN_RATE, is_buy_price, pass_lower_check)
                                         stdout_write(str)
-                                        if below_buy_level and over_buy_time \
-                                            and TRACE_MIN_PRICE > 0 and not over_max_buy_price:
+                                        if below_buy_level and over_buy_time and pass_lower_check and TRACE_MIN_PRICE > 0 and not over_max_buy_price:
                                             fa.write(str+'\n')
                                         # 买入条件：仓位、时间、达到分钟内的最低价、反弹至指定高度、成本均价是否越买越低
                                         if below_buy_level and over_buy_time and is_buy_price \
